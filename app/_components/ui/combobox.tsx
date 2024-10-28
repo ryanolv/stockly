8d1943aacf23b4a8807ce899606d6af5db8d4885
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-10-28 01:21:02 -0300

    feat: add initial sale creation component

---
+++ app/_components/ui/combobox.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/app/_lib/utils";
+import { Button } from "@/app/_components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/app/_components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/app/_components/ui/popover";
+
+export interface ComboboxOption {
+  value: string;
+  label: string;
+}
+
+interface ComboboxProps {
+  options: ComboboxOption[];
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+}
+
+export function Combobox({
+  value,
+  options,
+  placeholder,
+  onChange,
+}: ComboboxProps) {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full justify-between"
+        >
+          {value
+            ? options.find((option) => option.value === value)?.label
+            : placeholder}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search framework..." />
+          <CommandList>
+            <CommandEmpty>No framework found.</CommandEmpty>
+            <CommandGroup>
+              {options?.map((option) => (
+                <CommandItem
+                  key={option.value}
+                  value={option.value}
+                  onSelect={(currentValue) => {
+                    onChange(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === option.value ? "opacity-100" : "opacity-0",
+                    )}
+                  />
+                  {option.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,5 +1,41 @@
-const SalesPage = () => {
-  return <h1>Sales Page</h1>;
+import { PlusIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { ComboboxOption } from "../_components/ui/combobox";
+import { Sheet, SheetTrigger } from "../_components/ui/sheet";
+import { getProducts } from "../_data-access/product/get-products";
+import UpsertSheetContent from "./_components/upsert-sheet-content";
+
+const SalesPage = async () => {
+  const products = await getProducts();
+  const options: ComboboxOption[] = products.map((product) => {
+    return {
+      value: product.id,
+      label: product.name,
+    };
+  });
+  return (
+    <div className="w-full space-y-5 p-8">
+      <div className="mt-8 flex items-end justify-between">
+        <div className="space-y-2">
+          <span>Produtos</span>
+          <h2 className="text-2xl font-bold">Gestão de Vendas</h2>
+        </div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className="gap-1">
+              <PlusIcon size={20} />
+              Adicionar Venda
+            </Button>
+          </SheetTrigger>
+          <UpsertSheetContent products={products} productOptions={options} />
+        </Sheet>
+      </div>
+      {/* <DataTable
+        columns={columns}
+        data={JSON.parse(JSON.stringify(products))}
+      /> */}
+    </div>
+  );
 };
 
 export default SalesPage;
