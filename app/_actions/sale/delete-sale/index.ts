790135b7f12417df133af21351cf8dc4880729fd
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-11-23 00:06:50 -0300

    feat: ensure product stock is restored after sale is deleted

--- app/_actions/sale/delete-sale/index.ts
+++ app/_actions/sale/delete-sale/index.ts
@@ -6,8 +6,29 @@ import { db } from "@/app/_lib/prisma";
 
 export const deleteSale = async ({ id }: DeleteSaleSchema) => {
   deleteSaleSchema.parse({ id });
-  await db.sale.delete({
-    where: { id },
+  await db.$transaction(async (trx) => {
+    const sale = await trx.sale.findUnique({
+      where: { id },
+      include: {
+        products: true,
+      },
+    });
+    if (!sale) throw new Error("Sale not found");
+    await trx.sale.delete({
+      where: { id },
+    });
+    for (const product of sale.products) {
+      await trx.product.update({
+        where: { id: product.productId },
+        data: {
+          stock: {
+            increment: product.quantity,
+          },
+        },
+      });
+    }
   });
   revalidatePath("/sales");
+  revalidatePath("/products");
+  revalidatePath("/");
 };
